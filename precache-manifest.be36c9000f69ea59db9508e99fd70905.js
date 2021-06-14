a898187c90d9915552c30c2bbd41dee2ecd5559f
vincent <vincentvanderstruis@icloud.com>
2021-06-14 17:30:38 +0200

    Updates

---
+++ precache-manifest.be36c9000f69ea59db9508e99fd70905.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b604b186b1dcb0f9b32ac86ae0a754",
+    "url": "/FileStore/index.html"
+  },
+  {
+    "revision": "0dc0ed79ace9ffb24a25",
+    "url": "/FileStore/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "6463e9ef66681869babc",
+    "url": "/FileStore/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "0dc0ed79ace9ffb24a25",
+    "url": "/FileStore/static/js/2.4af2627f.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/FileStore/static/js/2.4af2627f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6463e9ef66681869babc",
+    "url": "/FileStore/static/js/main.fc872659.chunk.js"
+  },
+  {
+    "revision": "0da122897a385cdd3c86",
+    "url": "/FileStore/static/js/runtime-main.387a744e.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/FileStore/static/media/box.df8b84d5.png"
+  }
+]);
